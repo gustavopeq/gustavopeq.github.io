1051b89935b5fd915b0b3a83062f7f203619405b
gustavopeq <gustavo_pequeno2@hotmail.com>
2020-01-20 16:33:33 -0800

    Javascript added. Gifs added(SxS2 and Warsteroid)

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function()
+{
+    $('.card-img-top').mouseover(function()
+    {
+        if(this.classList.contains("hasGif"))
+        {
+            var gifSrc = $(this).attr("src").slice(0,-3) + "gif";
+            $(this).attr("src", gifSrc);
+        }
+        
+    }).mouseout(function()
+    {
+        var jpgSrc = $(this).attr("src").slice(0,-3) + "jpg";
+        $(this).attr("src", jpgSrc);
+    });
+});
